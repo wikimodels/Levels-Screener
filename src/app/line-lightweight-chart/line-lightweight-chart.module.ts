c64514bffc402f6a741e9031e1fd6cbecf3c7bc9
wikimodels <young.wiki.models@gmail.com>
2025-04-17 02:22:16 +0300

    done with lightweight charts

--- src/app/line-lightweight-chart/line-lightweight-chart.module.ts
+++ src/app/line-lightweight-chart/line-lightweight-chart.module.ts
@@ -1,13 +1,21 @@
+// line-lightweight-chart.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { LineLightweightChartComponent } from './line-lightweight-chart.component';
+import { LineLightweightChartRoutingModule } from './line-lightweight-chart-routing.module';
 
 @NgModule({
   declarations: [LineLightweightChartComponent],
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatTooltipModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    LineLightweightChartRoutingModule,
+  ],
   exports: [LineLightweightChartComponent],
 })
 export class LineLightweightChartModule {}
--- src/app/vwap-lightweight-chart/vwap-lightweight-chart.module.ts
+++ src/app/vwap-lightweight-chart/vwap-lightweight-chart.module.ts
@@ -4,10 +4,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { VwapLightweightChartComponent } from './vwap-lightweight-chart.component';
+import { VwapLightweightChartRoutingModule } from './vwap-lightweight-chart-routing.module';
 
 @NgModule({
   declarations: [VwapLightweightChartComponent],
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatTooltipModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    VwapLightweightChartRoutingModule,
+  ],
   exports: [VwapLightweightChartComponent],
 })
 export class VwapLightweightChartModule {}
